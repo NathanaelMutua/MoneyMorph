5af7049bcef993229c995dd74f1cf01c11474744
Nxsh <nathanael.mutua.m@gmail.com>
2025-06-10 21:36:08 +0300

    add and test currency conversion API - add HTML content for the currency conversion section - add form to allow user input for currency conversion - update main.js file to have logic for DOM and API fetching - add basic styling for conversion section, for readability

--- main.js
+++ main.js
@@ -1,24 +1,25 @@
-import "./style.css";
-import javascriptLogo from "./javascript.svg";
-import viteLogo from "/vite.svg";
-import { setupCounter } from "./counter.js";
+const baseCurrency = document.querySelector(".base-currency");
+const baseCurrencyValue = baseCurrency.value; // holds the input value for the base currency type
+const baseAmount = document.querySelector(".base-amount");
+const baseAmountValue = baseAmount.value; // holds the input value for the base conversion amount
+const conversionCurrency = document.querySelector(".conversion-currency");
+const conversionCurrencyValue = conversionCurrency.value; // will hold the input value for the conversion-to currency
+const convertBtn = document.getElementById("convert-button");
 
-document.querySelector("#app").innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`;
 
-setupCounter(document.querySelector("#counter"));
+convertBtn.addEventListener("click", async function () {
+  const conversionCurrencyValue = conversionCurrency.value; // will hold the input value for the conversion-to currency
+  const baseCurrencyValue = baseCurrency.value; // will hold the input value for the base currency type
+  const baseAmountValue = baseAmount.value; // will hold the input value for the base conversion amount
+  try{
+    const response = await fetch(
+      `https://api.unirateapi.com/api/convert?api_key=XL89LFgeWPhfIwgPt0z3KEeZjile4bIebTJajctKWSyJ5UifiP0SZcOrNNJl7JpG&amount=${baseAmountValue}&from=${baseCurrencyValue}`
+    );
+
+    const result = await response.json();
+
+    console.log(result);
+  } catch (error) {
+    console.error();    
+  }
+})
